6797ce26a4bff7dd3f3a8c27c0df18e7c33cd070
stwer1998 <stwer1998@mail.ru>
2020-02-29 16:57:58 +0300

    First version

---
+++ ExamDotNet/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ExamDotNet.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
